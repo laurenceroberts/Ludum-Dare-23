7fe0b9d11b33053397abab6b0a4edf934c500dfb
Laurence Roberts <info@gelatindesign.co.uk>
2012-04-21 13:03:40 +0100

    Platforming fixes

--- game/Player.py
+++ game/Player.py
@@ -1,6 +1,7 @@
 import pygame
 from Game import Game
 from AnimatedSprite import AnimatedSprite
+from Sprite import Sprite
 
 # Player
 class Player( AnimatedSprite ):
@@ -13,8 +14,11 @@ class Player( AnimatedSprite ):
 	
 	speed_X = 5
 	speed_Y = 8
+	accel_X = 0.4
 	move_X = 0
 	move_Y = 0
+	
+	feet = None
 	
 	collisions = {'Platform': []}
 	
@@ -25,15 +29,17 @@ class Player( AnimatedSprite ):
 		self.addAnimState( "move",	0, 3, 6 )
 		
 		self.setAnimState( "move" )
-	
-	#def spriteRect( self ):
-	#	r = self.image.get_rect( )
-	#	return pygame.Rect( 20, 40, 5, 4 )
+		
+		# Add player feet collider
+		#self.feet = PlayerFeet( self )
+		#Game.addSprite( "player", self.feet )
 	
 	def draw( self, screen, frame_ticks, ticks, fps ):
 		# Move
 		self.pos[0] += self.move_X
 		self.pos[1] += self.move_Y
+		
+		#self.feet.update( )
 		
 		# Animation
 		self.updateAnim( ticks )
@@ -86,7 +92,6 @@ class Player( AnimatedSprite ):
 				self.collisions[collisions[i].__class__.__name__].append( collisions[i] )
 	
 	def physics( self ):
-		
 		# Apply gravity
 		if self.move_Y < Game.gravity:
 			self.move_Y += Game.gravity_a
@@ -96,6 +101,44 @@ class Player( AnimatedSprite ):
 		# Check platforms
 		plength = len(self.collisions['Platform'])
 		if plength > 0:
-			if self.move_Y > 0:
-				self.move_Y = 0
-			
+			for i in range(0, plength):
+				platform = self.collisions['Platform'][i]
+				
+				if platform.rect.y > self.rect.y: # platform underneath
+					if platform.rect.y < self.rect.y + self.rect.height: # player clipping platform
+						self.pos[1] = platform.rect.y - self.rect.height + 1
+						#self.move_Y = self.move_Y * 0.5
+					
+					if self.move_Y > 0:
+						self.move_Y = 0
+				else: # platform above
+					if self.move_Y < 0:
+						pass
+						#self.move_Y = 0
+				
+'''
+class PlayerFeet( Sprite ):
+	player = None
+	
+	def __init__( self, player ):
+		#self.rect = pygame.Rect( 0, 0, 10, 4 )
+		super( PlayerFeet, self ).__init__( [0,0], "sprites/player/player-feet.png" )
+		
+		self.player = player
+	
+	def draw( self, screen, frame_ticks, ticks, fps ):
+		super( PlayerFeet, self ).draw( screen, frame_ticks, ticks, fps )
+		return ["check-collisions", "world"]
+	
+	def update( self ):
+		self.pos[0] = self.player.pos[0] + 4
+		self.pos[1] = self.player.pos[1] + 45
+		
+	def collisionsListener( self, collisions ):
+		self.player.collisions = {'Platform': []}
+		length = len( collisions )
+		print collisions
+		if length > 0:
+			for i in range( 0, length ):
+				self.player.collisions[collisions[i].__class__.__name__].append( collisions[i] )
+'''
